71f36037f24d9d630a99ff993218508316ed4d77
dhoffmanvillage <72418110+dhoffmanvillage@users.noreply.github.com>
2021-04-01 08:54:20 -0700

    Update Main.java

--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -1,9 +1,6 @@
 package u4pp;
-import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        RockPaperScissors.play(sc);
     }
 }
